beb18549bfa8aa1d7b3b233d0fb3389c5128f842
Maxsmile123 <sysoevmaksim27@gmail.com>
2020-12-12 13:27:39 +0300

    Delete token

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
+        // There must be Tokens
 
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
         long startTime = System.currentTimeMillis();
